9e77b8c309fd8f0e803915aa005b2a2f08d3a4aa
Nyi-Nyi-Zin <1.4052462e+08+Nyi-Nyi-Zin@users.noreply.github.com>
2026-08-17 09:17:50 +0000

    deploy: publish Myanmar agent selectors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3373033994" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4069250676" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
