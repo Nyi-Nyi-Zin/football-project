814a6929da19ea779ffa1f13eb38d24914f564b7
Nyi-Nyi-Zin <1.4052462e+08+Nyi-Nyi-Zin@users.noreply.github.com>
2026-08-18 15:02:43 +0000

    deploy: publish agent app from d7e4ccc

--- agent/flutter_bootstrap.js
+++ agent/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "934751318" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2331444753" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
